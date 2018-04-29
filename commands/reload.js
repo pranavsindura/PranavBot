13db96cc3b64c80ab918f29be8daf10d4190e9ee
Pranav Sindura <pranav.sindura@gmail.com>
2018-04-29 21:14:21 +0530

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const config = require('/PranavBot/config.json');
+const config = require('./config.json');
 const sql = require('sqlite3');
-const db = new sql.Database('/pranavbot/points.sqlite');
+const db = new sql.Database('./points.sqlite');
 const util = require('util');
 
 db.get = util.promisify(db.get);
@@ -15,19 +15,11 @@ exports.run = (client, message, args) =>
     
     if(message.author.id === config.ownerID)
     {
-        if(!args.length || args.length >1)
-        {
-            //message.channel.send("Please mention the command to reload.\n"+config.prefix+"reload <command>");
-            message.channel.send(new Discord.RichEmbed()
-            .setColor(0x00ae86)
-            .setTitle("Usage:")
-            .setDescription("**`" + row.prefix + "reload <command>`**"));
-        }
-        else
-        {
-            delete require.cache[require.resolve(`./${args[0]}.js`)];
-            message.channel.send("Reloaded");
-        }
+    	delete require.cache[require.resolve(`./${args[0]}.js`)];
+        message.channel.send("Reloaded").then((m) =>
+	{
+		setTimeout(() => {m.delete()},3000);
+	});
     }
     else
     {
