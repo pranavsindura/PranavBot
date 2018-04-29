9183cf1cf703eeb5542276ca88446e27f1be1bbc
Pranav Sindura <pranav.sindura@gmail.com>
2018-04-29 21:06:17 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const config = require("./config.json");
 const sql = require('sqlite3');
-const db = new sql.Database('/pranavbot/points.sqlite');
+const db = new sql.Database('./points.sqlite');
 const util = require('util');
 
 db.run = util.promisify(db.run);
@@ -15,43 +15,6 @@ exports.run = (client, message) =>
 	.then((row) => 
 	{
 		//Adding Points
-/*
-	if(!message.author.bot && message.channel.type !== "dm")
-	{
-		//Tag help
-		
-		if(message.mentions.members.some((m) => m.id == config.myID) && message.content.includes("help"))
-		{
-			message.channel.send(new Discord.RichEmbed()
-			.setColor(0x00ae86)
-			.setTitle("Help Menu")
-			.addField("Current Prefix", row.prefix)
-			.addField("For More Info","type **`" + row.prefix + "help`**"));
-		}
-
-		db.get(
-			`select * from Members where id = "${message.author.id}";`)
-		.then((row) =>
-		{
-			if(!row)
-			{
-				db.run(
-					`Insert into Members values ("${message.author.username}", "${message.author.id}", 0);`)
-					.catch(err => console.log(err));
-			}
-			else
-			{
-				db.run(
-						`Update Members set points = points+1 where id = "${message.author.id}";`)
-						.catch(err => console.log(err));
-			}
-		})
-		.catch((err) => 
-		{
-			console.log(err);
-		});
-	}
-*/
 	//Handling Messages
 	if(message.author.bot || message.channel.type === "dm")
 	{
