d51bcbe173c5dedb9216e1f5e792cb9cc0f6a4d3
Pranav Sindura <pranav.sindura@gmail.com>
2018-04-29 20:37:42 +0530

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require('/PranavBot/config.json');
+const config = require('/config.json');
 const fs = require('fs');
 
 const client = new Discord.Client();
