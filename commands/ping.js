2f7e1b66ef2e46d8ac520886504a8324baa5ab43
Pranav Sindura <pranav.sindura@gmail.com>
2018-04-30 00:41:49 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 const sql = require('sqlite3');
-const db = new sql.Database('./points.sqlite');
+const db = new sql.Database('./sqldb/points.sqlite');
 const util = require('util');
 
 db.get = util.promisify(db.get);
