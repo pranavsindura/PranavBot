3f0c3c01cc1e9dd4ac226e7db62af6513aa796b1
Pranav Sindura <pranav.sindura@gmail.com>
2018-04-30 00:44:34 +0530

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+
 const sql = require('sqlite3');
-const db = new sql.Database('./points.sqlite');
+const db = new sql.Database('./sqldb/points.sqlite');
 const util = require('util');
 
 db.get = util.promisify(db.get);
@@ -13,7 +13,7 @@ exports.run = (client, message, args) =>
 	.then((row) => 
 	{
     
-    if(message.author.id === config.ownerID)
+    if(message.author.id === process.env.ownerID)
     {
     	delete require.cache[require.resolve(`./${args[0]}.js`)];
         message.channel.send("Reloaded").then((m) =>
