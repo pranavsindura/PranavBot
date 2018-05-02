b29d503afe7beadabdeea1cab4b10a71c0d14726
Pranav Sindura <pranav.sindura@gmail.com>
2018-05-02 15:55:10 +0530

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,15 +1,10 @@
 const Discord = require("discord.js");
 const fs = require('fs');
 const client = new Discord.Client();
-
-<<<<<<< HEAD
 const path = require('path');
 global.appDir = path.resolve(__dirname);
 
 fs.readdir(appDir + '/events/', (err, files) => 
-=======
-fs.readdir('./events/', (err, files) => 
->>>>>>> b6446e51656f27e157189a542fb33999983f83ac
 {
 	if (err)
 	{
@@ -28,8 +23,5 @@ fs.readdir('./events/', (err, files) =>
 	});
 });
 
-<<<<<<< HEAD
 client.login(process.env.token);
-=======
-client.login(process.env.BOT_TOKEN);
->>>>>>> b6446e51656f27e157189a542fb33999983f83ac
+
